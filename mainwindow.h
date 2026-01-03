d83b106867aaaa409491e40aaf0c8c4fbe8d5042
kemoss000 <kemoss@qq.com>
2026-01-03 20:06:12 +0800

    Add files via upload

---
+++ mainwindow.h
@@ -0,0 +1,13 @@
+// MainWindow.h
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTabWidget>
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+public:
+    MainWindow(QWidget *parent = nullptr);
+};
+#endif
